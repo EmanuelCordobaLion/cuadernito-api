67769dcc4bece25ffdfcf558a427fe26ba4a203d
Emanuel Córdoba Lion <ema_cb@hotmail.com>
2026-01-23 11:43:53 -0300

    Initial commit: Spring Boot backend setup

---
+++ cuadernito-back/src/main/java/com/cuadernito/cuadernito_back/CuadernitoBackApplication.java
@@ -0,0 +1,13 @@
+package com.cuadernito.cuadernito_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuadernitoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuadernitoBackApplication.class, args);
+	}
+
+}
